a4743504ff450689d9b42aa7213c7a78d4c01f75
Dhruv Anurag <dhruvanurag@wirelessprv-10-192-22-236.near.illinois.edu>
2025-02-28 15:38:42 -0600

    included metrics used for model

---
+++ stats.py
@@ -0,0 +1,21 @@
+'''
+Over the past 10 years, 16 out of the past 20 NBA finalists ranked top 5 in 2p%, 15 ranked top 5 in FG%, 12 ranked top
+ 5 in FG and 11 ranked top 5 in 3P. These are the 4 per game statistics which seemed to best predict the likelyhood of
+ a team making the NBA Finals. The best shooting teams tend to go far in the playoffs.
+
+ Using advanced statistics, 19 out of 20 of the past NBA finalists were top 5 in eFG%, 17 were top 5 in average Margin
+ of Victory and top 5 in net rating, and 13 were top 5 in offensive rating. These metrics make it clear that a team's 
+ efficiency from the field as well as their regular season dominance were conducive to reaching the NBA finals, and it
+ appears as if offensive rating matters more than defensive rating.
+
+ In recent times, teams have been obsessed with getting the most efficient shots in the playoffs, three pointers, rim
+ shots, or free throws. This new emergence of statistics in basketball has taken the NBA by storm, and some fans 
+ believe that maximizing efficiency during the regular season doesn't necessarily lead to playoff success. Analyzing 
+ shooting statistics over the past 10 years, we find that 16 of the past 20 teams were top 5 in 2p%, 15 were top 5 in
+ percentage at the rim, and 12 were top 5 in percentage from 16 ft to the 3 pt line, while 11 were top 5 in percentage
+ from the 3 point line. It's clear that three point percentage isn't necessarily the main factor for winning, and that
+ the midrange game is still important in the playoffs. 
+
+ We will be using 2p%, FG%, 3P, eFG%, Net RTG, margin of victory, offensive rating, percentage at the rim, and 
+ percentage from 16 ft - 3 pt line in our model to predict the NBA champion. 
+'''
